67021697a396f477a889511c4a24d78cd6b7c0d3
abby <abby.greenbloom@gmail.com>
2020-09-06 15:53:00 -0400

    delete content and add contact modal

--- js/main.js
+++ js/main.js
@@ -1,14 +1,32 @@
-$(document).ready(function () {
-    $(".NavBtn").on("click", function () {
-        $(".NavBtn").toggleClass("change");
-        if ($(".NavBtn").hasClass("change")) {
-            $("#MenuPanel").animate({ left: 0 }, 1000);
-            $('html,body').animate({
-                scrollTop: 0
-            }, 1000);
-            
-        } else {
-            $("#MenuPanel").animate({ left: '-100%' }, 1000);
-        };
-    });
+document.addEventListener("DOMContentLoaded", function(){
+  // Handler when the DOM is fully loaded
+
+
+  // Get the modal
+  var modal = document.getElementById("modal");
+console.log(modal)
+  // Get the button that opens the modal
+  var btn = document.getElementById("contactBtn");
+
+  // Get the <span> element that closes the modal
+  // var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks the button, open the modal
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  // span.onclick = function() {
+  //   modal.style.display = "none";
+  // }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+
 });
